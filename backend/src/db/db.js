92c910ec26b51c351f187f7e81ce082235a28374
Kanak Yadav <yadavkanak109@gmail.com>
2026-05-03 10:15:12 +0530

    base setup

---
+++ backend/src/db/db.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+
+
+const UserSchema = new mongoose.Schema({
+
+    
+});
+
+
+
+const UserModel = mongoose.model('User', UserSchema);
+
+
+
+module.exports = UserModel;
